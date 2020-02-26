8dc08daa80af6f2597c4525fbadaa925f649966d
Easlee Liu <easlee@outlook.com>
2020-02-26 10:28:12 +0000

    update

--- install-server.sh
+++ install-server.sh
@@ -14,7 +14,7 @@ cp ./config/consul.service /etc/systemd/system/
 cp ./config/consul.hcl /etc/consul.d/
 chmod 640 /etc/consul.d/consul.hcl
 
-cp ./config/service.hcl /etc/consul.d/
+cp ./config/server.hcl /etc/consul.d/
 chmod 640 /etc/consul.d/server.hcl
 
 systemctl enable consul
